170151009424407ba70e531d956ae82b2704f303
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-12-09 11:40:23 +0100

    Vererbung & Naming

--- src/lecture/chapter6/FarmRunExample.java
+++ src/lecture/chapter6/FarmRunExample.java
@@ -38,5 +38,73 @@ public class FarmRunExample {
         System.out.println(myTweety.toString());
 
         System.out.println(myBello);
+
+
+        // narrowing Cast --> einschränken der Sichtbarkeit auf Objekte
+
+        Animal animalOne = myBello;
+        Animal animalTwo = myTweety;
+
+        myBello.bark();
+        myBello.move();
+
+        // animalOne.bark(); --> nicht nutzbar über Animal Referenz
+        animalOne.move();
+        animalOne.eat();
+        animalOne.breath();
+
+        // animalTwo.tweet(); --> nicht nutzbar über Animal Referenz
+        animalTwo.move();
+        animalTwo.eat();
+        animalTwo.breath();
+
+        // widening Cast --> erweitern der Sichtbarkeit auf ein Objekt
+
+        if(animalOne instanceof Dog){
+            Dog dogOne = (Dog)animalOne;
+
+            dogOne.bark();
+            dogOne.move();
+
+            System.out.println("Rasse: " + dogOne.getBreed());
+        }
+
+        if(animalTwo instanceof Dog){
+            Dog dogTwo = (Dog)animalTwo;
+
+            dogTwo.bark();
+            dogTwo.move();
+        }else{
+            System.out.println("animalTwo ist KEIN Hund!");
+        }
+
+
+        // Polymorphy
+
+        Animal[] animalBox = new Animal[5];
+
+        // Narrowing Cast von verschiedenen Tieren auf "Animal"
+        animalBox[0] = myBello;
+        animalBox[1] = myTweety;
+        animalBox[2] = new Dog(67, "Hasso", 15, "Mischling");
+        animalBox[3] = new Bird(17, "Birdy", 3, true);
+        animalBox[4] = new Bird(30, "Pingu", 4, false);
+
+        System.out.println("Bauer läuft durch den Stall und kümmert sich um die Tiere:");
+        for(Animal currentAnimal : animalBox){
+            currentAnimal.eat();
+            currentAnimal.move();
+            currentAnimal.breath();
+
+            if(currentAnimal instanceof Dog){
+                Dog currentDog = (Dog)currentAnimal;
+                currentDog.bark();
+            }
+
+            System.out.println(currentAnimal);
+        }
+
+
+
     }
 }

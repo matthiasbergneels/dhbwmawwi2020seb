a9ce366c0de560355bac39aba9fc6f61ff09155b
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2021-06-01 17:21:01 +0200

    InnerClasses

---
+++ src/lecture/excursion/innerclasses/OuterClass.java
@@ -0,0 +1,50 @@
+package lecture.excursion.innerclasses;
+
+public class OuterClass {
+
+    public static interface InnerInterface{
+        void print(String message);
+    }
+
+    // innere Top-Level Klasse
+    public static class InnerTopLevelClass{
+        void print(String message){
+            System.out.println(InnerTopLevelClass.class.getName() + " - Innere Top Level Klasse sagt: " + message);
+        }
+    }
+
+    // inner Elemente Klasse
+    public class InnerElementClass{
+        void print(String message){
+            System.out.println(InnerElementClass.class.getName() + " - Innere Element Klasse sagt: " + message);
+        }
+    }
+
+    void printFromInnerLocalClass(String message){
+        // innere lokale Klasse
+        class InnerLocalClass{
+            void print(String message){
+                System.out.println(InnerLocalClass.class.getName() + " - Innere Lokale Klasse sagt: " + message);
+            }
+        }
+
+        InnerLocalClass myInnerLocalClass = new InnerLocalClass();
+        myInnerLocalClass.print(message);
+    }
+
+    public static void main(String[] args) {
+        String message = "Hello World";
+
+        System.out.println(OuterClass.class.getName() + " - Äusere Klasse sagt: " + message);
+
+        OuterClass.InnerTopLevelClass myInnerTopLevelClass = new OuterClass.InnerTopLevelClass();
+        myInnerTopLevelClass.print(message);
+
+        OuterClass myOuterClass = new OuterClass();
+        InnerElementClass myInnerElementClass = myOuterClass.new InnerElementClass();
+        myInnerElementClass.print(message);
+
+        myOuterClass.printFromInnerLocalClass(message);
+
+    }
+}

19afd9bee3b76dd7364226c6711e314a4ffd0494
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2021-07-20 16:09:05 +0200

    Sortalgorithms TestClass

--- src/lecture/chapter13/SortAlgorithms.java
+++ src/lecture/chapter13/SortAlgorithms.java
@@ -2,69 +2,106 @@ package lecture.chapter13;
 
 public class SortAlgorithms {
 
+    private static int swapCount = 0;
+    private static int getSwapCount = 0;
+
     public static void main(String[] args) {
         int[] unsortedNumbers = {50, 30, 80, 40, 60, 100, 20, 70, 10, 90};
 
-        System.out.println("Bubble Sort I:");
         int[] sortedNumbers = bubbleSortNumberArray(unsortedNumbers.clone());
         printArray(sortedNumbers);
 
-        System.out.println("Bubble SortII:");
-        sortedNumbers = bubbleSortNumberArray(unsortedNumbers.clone());
+        sortedNumbers = bubbleSortNumberArrayV2(unsortedNumbers.clone());
+        printArray(sortedNumbers);
+
+        sortedNumbers = bubbleSortNumberArrayV3(unsortedNumbers.clone());
         printArray(sortedNumbers);
 
     }
 
-
-
-    public static int[] bubbleSortNumberArray(int numbers[]){
+    public static int[] bubbleSortNumberArray(int numbers[]) {
 
         long startTime = System.currentTimeMillis();
 
-        for(int i = numbers.length; i > 0; i--){
-            for(int j = 0; j < (numbers.length - 1); j++){
-                if(numbers[j] > numbers[j+1]){
+        // O(n*n)
+        for (int i = numbers.length; i > 0; i--) {
+            for (int j = 0; j < (numbers.length - 1); j++) {
+                if (numbers[j] > numbers[j + 1]) {
                     // tausche Elemente
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
+                    swapElementsInArray(numbers, j);
                 }
             }
         }
 
         long endTime = System.currentTimeMillis();
 
-        System.out.println("Bubble Sort Runtime: " + (endTime - startTime) + " ms");
+        System.out.println("Bubble Sort Runtime: " + (endTime - startTime) + " ms - SwapCount: " + swapCount);
+        swapCount = 0;
 
         return numbers;
     }
 
-    public static int[] bubbleSortNumberArrayV2(int numbers[]){
+    public static int[] bubbleSortNumberArrayV2(int numbers[]) {
 
         long startTime = System.currentTimeMillis();
 
-        for(int i = 0; i < numbers.length; i++){
-            for(int j = 0; j < (numbers.length - 1 - i); j++){
-                if(numbers[j] > numbers[j+1]){
+        // O(n * log n)
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < (numbers.length - 1 - i); j++) {
+                if (numbers[j] > numbers[j + 1]) {
                     // tausche Elemente
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
+                    swapElementsInArray(numbers, j);
                 }
             }
         }
 
         long endTime = System.currentTimeMillis();
 
-        System.out.println("Bubble Sort II Runtime: " + (endTime - startTime) + " ms");
+        System.out.println("Bubble Sort II Runtime: " + (endTime - startTime) + " ms - SwapCount: " + swapCount);
+        swapCount = 0;
 
         return numbers;
     }
 
-    private static void printArray(int[] numbers){
-        System.out.println("Array Ausgabe: ");
-        for(int i : numbers){
-            System.out.print(i + ", ");
+    public static int[] bubbleSortNumberArrayV3(int numbers[]) {
+
+        long startTime = System.currentTimeMillis();
+
+        int i = numbers.length - 1;
+        boolean swap = false;
+
+        // O(n * log n) --> optimiert für teilweise sortierte Arrays
+        do {
+            swap = false;
+            for (int j = 0; j <= i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    // tausche Elemente
+                    swapElementsInArray(numbers, j);
+                    swap = true;
+                }
+            }
+            i--;
+        } while (swap);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Bubble Sort III Runtime: " + (endTime - startTime) + " ms - SwapCount: " + swapCount);
+        swapCount = 0;
+
+        return numbers;
+    }
+
+    private static void swapElementsInArray(int[] numbers, int j) {
+        swapCount++;
+        int temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+    }
+
+    private static void printArray(int[] numbers) {
+        System.out.print("Array Ausgabe: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ( (i < numbers.length - 1)  ? ", " : ""));
         }
         System.out.println();
     }
---
+++ src/test/lecture/chapter13/SortAlgorithmsTest.java
@@ -0,0 +1,200 @@
+package test.lecture.chapter13;
+
+import lecture.chapter13.SortAlgorithms;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortAlgorithmsTest {
+
+    static int count = 20000;
+    static int randomRange = 3000000;
+    static int[] toSort = null;
+
+    @DisplayName("Testing on unsorted Array")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class UnsortedArrays {
+
+        @BeforeAll
+        void setUp() {
+            toSort = generatedArrayWithRandomValues();
+        }
+
+        @Test
+        void bubbleSortNumberArray() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArray(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArrayV2() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV2(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArrayV3() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV3(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArrayV3TimeBased() {
+            assertTimeout(Duration.ofMillis(500), () -> {
+                int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV3(toSort.clone());
+            });
+
+        }
+    }
+
+
+    @DisplayName("Testing on sorted Array")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class SortedArrays {
+
+        @BeforeAll
+        void setUp() {
+            toSort = generateSortedArray();
+        }
+
+        @Test
+        void bubbleSortNumberArraySorted() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArray(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArraySortedV2() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV2(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArraySortedV3() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV3(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+    }
+
+
+    @DisplayName("Testing on half sorted Array (first half sorted)")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class FirstHalfSortedArrays {
+
+        @BeforeAll
+        void setUp() {
+            toSort = generateFirstHalfSortedArray();
+        }
+
+        @Test
+        void bubbleSortNumberArraySorted() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArray(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArraySortedV2() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV2(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArraySortedV3() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV3(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+    }
+
+
+    @DisplayName("Testing on half sorted Array (second half sorted)")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class SecondHalfSortedArrays {
+
+        @BeforeAll
+        void setUp() {
+            toSort = generateSecondHalfSortedArray();
+        }
+
+        @Test
+        void bubbleSortNumberArraySorted() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArray(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArraySortedV2() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV2(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+
+        @Test
+        void bubbleSortNumberArraySortedV3() {
+            int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV3(toSort.clone());
+            assertTrue(isSorted(bubbleSortedArray));
+        }
+    }
+
+    private static boolean isSorted(int[] array){
+        for(int i = 0; i < array.length-1; i++){
+            if(array[i] > array[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] generatedArrayWithRandomValues(){
+        int[] generatedArray = new int[count];
+        for(int i = 0; i < count; i++){
+            boolean notInserted = true;
+            while(notInserted){
+                int randomNumber = (int)(Math.random() * randomRange);
+
+                boolean found = false;
+                for(int j = 0; j < i; j++){
+                    if(generatedArray[j] == randomNumber){
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found){
+                    generatedArray[i] = randomNumber;
+                    notInserted = false;
+                }
+
+            }
+        }
+        return generatedArray;
+    }
+
+    private static int[] generateSortedArray(){
+        int[] generatedArray = new int[count];
+        for(int i = 0; i < count; i++){
+            generatedArray[i] = i;
+        }
+        return generatedArray;
+    }
+
+    private static int[] generateFirstHalfSortedArray(){
+        int[] generatedArray = generatedArrayWithRandomValues();
+        for(int i = 0; i < count/2; i++){
+            generatedArray[i] = i;
+        }
+        return generatedArray;
+    }
+
+    private static int[] generateSecondHalfSortedArray(){
+        int[] generatedArray = generatedArrayWithRandomValues();
+        for(int i = (int)count/2; i < count; i++){
+            generatedArray[i] = i;
+        }
+        return generatedArray;
+    }
+}
